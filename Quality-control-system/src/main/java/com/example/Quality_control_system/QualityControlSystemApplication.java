c46de3afe4575e220ec603df6fbf7c29a950b768
Bouchra <bouchraazrour2@gmail.com>
2024-07-29 00:14:03 +0200

    Add Quality-control-system files

---
+++ Quality-control-system/src/main/java/com/example/Quality_control_system/QualityControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quality_control_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityControlSystemApplication.class, args);
+	}
+
+}
---
+++ Quality-control-system/src/test/java/com/example/Quality_control_system/QualityControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quality_control_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
