b4d46ffaad417ff5f862ebfe42855342da3c5ab3
Bouchra <bouchraazrour2@gmail.com>
2024-09-03 13:06:36 +0200

    edit login page

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    const validUsername = 'Bouchra';
+    const validPassword = '123456';
+
+    if (this.username === validUsername && this.password === validPassword) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid username or password. Please try again.');
+    }
+  }
 }
